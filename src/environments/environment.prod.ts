cfde778f94e5ffd989f969891d10d7f14bbd39a4
YasminVarshaviak <yasmin1313@gmail.com>
2024-04-03 13:56:10 +0300

    ..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://my-arts-backend-d3c8d1c805b7.herokuapp.com/'
+  apiUrl: 'https://my-arts-backend-d3c8d1c805b7.herokuapp.com'
 
 };
